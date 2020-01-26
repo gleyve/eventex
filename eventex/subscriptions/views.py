2850cf1de4b9db9d312c5c6876b14ff54dbb9e61
gleyve <gleyverafael@gmail.com>
2020-01-25 21:54:34 -0300

    Realiza inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,4 +1,8 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from eventex.subscriptions.forms import SubscriptionForm
 
@@ -6,5 +10,25 @@ from eventex.subscriptions.forms import SubscriptionForm
 def subscribe(request):
     """return HttpResponse("Python Subscribe Test")"""
     """return render(request, 'subscriptions/subscription_form.html')"""
-    context = {'form': SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+
+        if form.is_valid():
+
+            body = render_to_string('subscriptions/subscription_email.txt',
+                                     form.cleaned_data)
+            mail.send_mail('Confirmação de inscrição',
+                           body,
+                           'contato@eventex.com.br',
+                           ['contato@eventex.com.br', form.cleaned_data['email']])
+
+            messages.success(request, 'Inscrição realizada com sucesso!')
+
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscriptions/subscription_form.html',
+                          {'form': form})
+
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
